1ddbb08fb6a99f304a070fa845a7c8be928ee2e6
Flabby Ninja <dev@flabbyninja.net>
2019-01-03 14:13:07 +0000

    Simple usage info

--- scripts/file_encrypt/remove_file_spaces.sh
+++ scripts/file_encrypt/remove_file_spaces.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#
+# Run like this to update files:
+#   find . -type f -name '* *' -exec remove_file_spaces.sh {} \;
 filename=$1
 new_filename=`echo $filename | sed -e 's/ /_/g'`
 mv "$filename" $new_filename
