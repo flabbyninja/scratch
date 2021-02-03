849c067c55d665c3028959ab58345a705f79def5
Flabby Ninja <dev@flabbyninja.net>
2021-02-03 13:33:46 +0000

    Initial loop and hashtable to configure targets

--- scripts/cleanup_anticheat/cleanup_anticheat.ps1
+++ scripts/cleanup_anticheat/cleanup_anticheat.ps1
@@ -1,2 +1,31 @@
-get-ciminstance win32_service -filter "name='EasyAntiCheat'" | remove-ciminstance
-get-ciminstance win32_service -filter "name='beservice'" | remove-ciminstance
+$targetServiceMap = @{
+  "Easy Anticheat" = "EasyAntiCheat"
+  "BattleEye" = "beservice"
+}
+
+foreach ($key in $targetServiceMap.Keys) {
+  $serviceName = $targetServiceMap[$key]
+  $message = "Processing service {0}({1})" -f $key, $serviceName
+  Write-Output $message    
+  $cimInstanceParams = @{
+      ClassName = "Win32_Service"
+      filter = "Name= '{0}'" -f $serviceName
+  }      
+  $serviceAvailable = get-ciminstance @cimInstanceParams
+  Write-Output $serviceAvailable
+}
+
+# $EasyAntiCheat = get-ciminstance win32_service -filter "name='EasyAntiCheat'" 
+#If ($EasyAntiCheat -ne $null) {
+  #Write-Output "EasyAnticheat service found"
+#}
+#else {
+#    Write-Output "EasyAnticheat service not found"
+#}
+# remove-ciminstance -InputObject $EasyAntiCheat
+# Write-Host $Result.ExitCode
+# $BattleEye = get-ciminstance win32_service -filter "name='beservice'" 
+# remove-ciminstance -InputObject $BattleEye
+# Write-Host $Result.ExitCode
+
+# Read-Host -Prompt "Press Enter to exit"
