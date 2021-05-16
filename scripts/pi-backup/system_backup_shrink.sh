37970b538f646972be993813d5a482c5c63ec61a
Flabby Ninja <dev@flabbyninja.net>
2021-05-16 15:46:17 +0100

    Usage statements, check for pishrink errors, argument parsing

--- scripts/pi-backup/system_backup_shrink.sh
+++ scripts/pi-backup/system_backup_shrink.sh
@@ -6,46 +6,62 @@
 # The script will name the backup files {$HOSTNAME}.{YYYYmmdd}.img
 # When the script deletes backups older then the specified retention
 # it will only delete files with it's own $HOSTNAME.
-#
 
 # Declare vars and set standard values
+shrink_image=true
 backup_path=/mnt/backup/raw
 retention_days=45
-pishrink=true
 
 # Set path to find common tools for piShrink - especially when run from crontab which uses a sparse, default environment
 PATH=$PATH:/usr/bin:/usr/sbin
 
-checkRoot() {
-   # Check that we are root!
-   if [[ ! $(whoami) =~ "root" ]]; then
-   echo ""
-   echo "**********************************"
-   echo "*** This needs to run as root! ***"
-   echo "**********************************"
-   echo ""
-   exit
-   fi
+display_usage() { 
+	echo -e "\nUsage: $0 [shrink_image] [backup_path] [retention_days]\n" 
 }
 
-checkRoot()
+checkRoot() {
+	if [[ "$EUID" -ne 0 ]]; then 
+		echo -e "\nError: This script must be run as root!\n" 
+		exit 1
+	fi 
+}
 
-# Check to see if we got command line args
+# if more than 3 arguments supplied, display usage 
+if [  $# -gt 3 ] 
+then 
+   echo -e "\nError: This script must have maximum of 3 parameters\n" 
+   display_usage
+   exit 1
+fi 
+
+# check whether user had supplied -h or --help . If yes display usage 
+if [[ ( $1 == "--help") ||  $1 == "-h" ]] 
+then 
+   display_usage
+   echo -e "Default values are shrink_image="$shrink_image, "backup_path="$backup_path ", retention_days="$retention_days"\n"
+   exit 0
+fi
+
+# Validate we're running as the root user
+checkRoot
+
+# Process command line arguments
 if [ ! -z $1 ]; then
-   backup_path=$1
+   shrink_image=$1
 fi
 
 if [ ! -z $2 ]; then
-   retention_days=$2
+   backup_path=$2
 fi
 
 if [ ! -z $3 ]; then
-   pishrink=$3
+   retention_days=$3
 fi
 
 backup_file=$backup_path/$HOSTNAME.$(date +%Y%m%d_%H%M%S).img
+
 echo $(date)
-echo 'Backing up Pi filesystem to' $backup_file
+echo 'Backing up Pi filesystem to' $backup_file", keeping "$retention_days" days of backups, and shrink is "$shrink_image
 
 # Create trigger to force file system consistency check if image is restored
 touch /boot/forcefsck
@@ -61,9 +77,23 @@ rm /boot/forcefsck
 # Delete old backups
 find $backup_path/$HOSTNAME.*.img -mtime +$retention_days -type f -delete
 
+runPiShrink() {
+   echo "Running command: " $piscriptdir/PiShrink/pishrink.sh -d $backup_file
+   $piscriptdir/PiShrink/pishrink.sh -d $backup_file
+}
+
 # shrink the image
-scriptdir=$(dirname $0)
-echo 'Invoking PiShrink to minimise image size...'
-echo "Running command: " $scriptdir/PiShrink/pishrink.sh -d $backup_file
-$scriptdir/PiShrink/pishrink.sh -d $backup_file
-echo 'Image shrinking complete'
+if [[ $shrink_image == "true" ]]
+then
+   piscriptdir=$(dirname $0)
+   echo 'Invoking PiShrink to minimise image size...'
+   if ! runPiShrink; then
+      echo 'Error shrinking image'
+   else
+      echo 'Image shrinking complete'
+   fi
+else 
+   echo "Image shrinking not enabled - skipping"
+fi
+
+echo "Image backup complete"
