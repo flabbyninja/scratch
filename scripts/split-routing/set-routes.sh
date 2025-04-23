d7d42de82ed4285328bdc9248f47623ee00b228c
Flabby Ninja <dev@flabbyninja.net>
2025-04-23 13:37:45 +0100

    Validate days traffic parameter is integer

--- scripts/split-routing/set-routes.sh
+++ scripts/split-routing/set-routes.sh
@@ -18,18 +18,24 @@ TRAFFIC_HISTORY_DAYS=5
 APP_NAME="/Library/Application Support/Citrix Receiver/Citrix Viewer.app/Contents/MacOS/Citrix Viewer"
 NET_INTERFACE=en0
 
+# Regexp to validate integer passed for traffic history days
+re='^[1-9][0-9]*$'
+
 # Parameters
 
 # Number of days traffic history to pull back. Default is 5
-if [ -z "$1" ]
+if [[ -z "$1" ]]
     then
-        TRAFFIC_HISTORY_DAYS=5
-    else
+        echo "No traffic history parameter set: using default of ${TRAFFIC_HISTORY_DAYS}"
+elif [[ $1 =~ $re ]]
+    then 
         TRAFFIC_HISTORY_DAYS=$1
+else 
+    echo "Provided traffic history parameter is not an integer (${1}): using default of ${TRAFFIC_HISTORY_DAYS}"
 fi
 
 # Subcommand for operation. Default is refresh (delete any route for IP, then add new route in)
-if [ -z "$2" ]
+if [[ -z "$2" ]]
     then
         SUBCOMMAND="refresh"
     else
@@ -53,16 +59,16 @@ echo "Traffic parsing previous $TRAFFIC_HISTORY_DAYS days (Start Time: $START_TI
 # remove existing rules if they exist for these IP's
 delete=0
 add=0
-for ip in $(littlesnitch log-traffic -b $START_TIME -e $END_TIME | grep "$APP_NAME" | cut -f 4 -d , | sort -u)
-do
-    route delete -net $ip $DEFAULT_GATEWAY
-    delete=$((delete+1))
-    if  [ -n $SUBCOMMAND ] && [ $SUBCOMMAND != "delete" ]
-    then
-        add=$((add+1))
-        route add -net $ip $DEFAULT_GATEWAY
-    fi
-done
+# for ip in $(littlesnitch log-traffic -b $START_TIME -e $END_TIME | grep "$APP_NAME" | cut -f 4 -d , | sort -u)
+# do
+#     route delete -net $ip $DEFAULT_GATEWAY
+#     delete=$((delete+1))
+#     if  [ -n $SUBCOMMAND ] && [ $SUBCOMMAND != "delete" ]
+#     then
+#         add=$((add+1))
+#         route add -net $ip $DEFAULT_GATEWAY
+#     fi
+# done
 
 # Output summary of what has been added and removed
 echo "IPs added: $add, IPs deleted: $delete"
