31d3fccfb636d33225a15b06dcd433953f2a6b8c
Flabby Ninja <dev@flabbyninja.net>
2025-12-05 15:37:36 +0000

    Add default sleep period config

--- scripts/split-routing/reset-networking.sh
+++ scripts/split-routing/reset-networking.sh
@@ -3,17 +3,21 @@
 # Remove any custom routes in route table. Must run as root, so use:
 # sudo <script>.sh
 NET_INTERFACE=en0
+DEFAULT_SLEEP_PERIOD=3
 
-echo "Resetting network route table..."
+echo "Resetting network route table... (sleep set to $DEFAULT_SLEEP_PERIOD)"
 
-for i in {1..3}
+for i in {1..5}
 do
     echo "Flushing routes: run " $i
     route -n flush
+    sleep $DEFAULT_SLEEP_PERIOD
 done
 
 echo "Bringing $NET_INTERFACE down"
 ifconfig $NET_INTERFACE down
+
+sleep $DEFAULT_SLEEP_PERIOD
 
 echo "Bringing $NET_INTERFACE up"
 ifconfig $NET_INTERFACE up
